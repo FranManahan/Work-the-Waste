58764c385ade897e2a1c2fa56248641a380cff05
Francesca Manahan <francescamanahan@gmail.com>
2023-06-13 09:30:20 -0500

    initial commit

--- Work the Waste/ContentView.swift
+++ Work the Waste/ContentView.swift
@@ -9,46 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(red: 0.23529411764, green: 0.38431372549, blue: 0.33333333333)
-                .ignoresSafeArea()
-            
-            VStack {
-                Image("wtw icon")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    .frame(width: 175.0, height: 175.0)
-                    .cornerRadius(1000)
+        
+        NavigationStack {
+            ZStack {
+                Color(red: 0.23529411764, green: 0.38431372549, blue: 0.33333333333)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Image("wtw icon")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: 175.0, height: 175.0)
+                        .cornerRadius(1000)
                     
-                Image("title")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 250.0)
-                
-                Button("Why Recycle?") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    Image("title")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250.0)
+                    
+                    Button("Why Recycle?") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     }
-                .buttonStyle(.borderedProminent)
-                .tint(.teal)
-
-                Button("Working the Waste") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    .buttonStyle(.borderedProminent)
+                    .tint(.teal)
+                    
+                    Button("Working the Waste") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     }
-                .buttonStyle(.borderedProminent)
-                .tint(.teal)
-                
-                Button("Sustainable Fashion") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    .buttonStyle(.borderedProminent)
+                    .tint(.teal)
+                    
+                    Button("Sustainable Fashion") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     }
-                .buttonStyle(.borderedProminent)
-                .tint(.teal)
-                
+                    .buttonStyle(.borderedProminent)
+                    .tint(.teal)
+                    
                 }
-                    
+                
             }
         }
-        
+    }
     }
 
 
